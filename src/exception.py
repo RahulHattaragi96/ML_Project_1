036dfc45f197bb9124d83f0050e18c8be9cba600
RahulHattaragi96 <rahulhattaragi96@gmail.com>
2024-02-16 19:48:31 +0530

    Exception_1

--- src/exception.py
+++ src/exception.py
@@ -8,7 +8,7 @@ def error_message_detail(error,error_detail:sys):
         file_name,exc_tb.tb_lineno,str(error)
     )
     return error_message
-    
+
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
@@ -18,6 +18,10 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
+if __name__=="__main__":
 
-
-
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by Zero")
+        raise CustomException(e,sys)
